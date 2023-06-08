2539468d68ff48b9251a774afd68aa101f272a41
gnilvan <gnilvan@users.noreply.github.com>
2023-06-08 23:23:38 +0200

    gh pages added to cors config

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-const fetch
+const mobNames = document.getElementsByClassName('mobNames');
+const mobNamesArr = [...mobNames];
+
+const getMobNames = async event => {
+  const mobName = event.target.id;
+  const mobDisplay = document.querySelector('#mobDisplay');
+  const response = await fetch(`http://localhost:4000/mobs/${mobName}/members`)
+  const mob = await response.json();
+  const mobMembers = mob;
+  const name = mobMembers.map(m => m.memberName);
+  mobDisplay.innerHTML = name;
+};
+
+mobNamesArr.forEach(mobName => mobName.addEventListener('click', getMobNames));
