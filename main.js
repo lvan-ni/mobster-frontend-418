b7f55863be032fc5c3d1712542d0e68ddffabd5a
gnilvan <gnilvan@users.noreply.github.com>
2023-06-08 23:39:20 +0200

    local and remote url added

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const mobNames = document.getElementsByClassName('mobNames');
 const mobNamesArr = [...mobNames];
 
+const URL = 'https://mobster-backend-418-production.up.railway.app';
+const devURL = 'http://localhost:4000';
+
 const getMobNames = async event => {
   const mobName = event.target.id;
   const mobDisplay = document.querySelector('#mobDisplay');
-  const response = await fetch(`http://localhost:4000/mobs/${mobName}/members`)
+  const response = await fetch(`${URL}/mobs/${mobName}/members`)
   const mob = await response.json();
   const mobMembers = mob;
   const name = mobMembers.map(m => m.memberName);
