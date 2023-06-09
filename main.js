11690a4341d4071e3d520b5b8d046f8ff6a341d7
gnilvan <gnilvan@users.noreply.github.com>
2023-06-09 13:53:05 +0200

    more styling added

--- main.js
+++ main.js
@@ -6,12 +6,27 @@ const devURL = 'http://localhost:4000';
 
 const getMobNames = async event => {
   const mobName = event.target.id;
-  const mobDisplay = document.querySelector('#mobDisplay');
-  const response = await fetch(`${URL}/mobs/${mobName}/members`)
+  console.log(mobName);
+  const nameCards = document.getElementById('nameCards');
+  const response = await fetch(`${devURL}/mobs/${mobName}/members`)
   const mob = await response.json();
   const mobMembers = mob;
   const name = mobMembers.map(m => m.memberName);
-  mobDisplay.innerHTML = name;
+//  console.log(mob);
+
+
+  const card1 = document.getElementById('nameCard1');
+  const card2 = document.getElementById('nameCard2');
+  const card3 = document.getElementById('nameCard3');
+  const card4 = document.getElementById('nameCard4');
+
+  card1.innerText = name[0];
+  card2.innerText = name[1];
+  card3.innerText = name[2];
+  card4.innerText = name[3];
+
 };
 
 mobNamesArr.forEach(mobName => mobName.addEventListener('click', getMobNames));
+
+
